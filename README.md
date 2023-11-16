789a49851c240e16dfac3abbf65df6a4f80b5dd4
elul <scorpiondota22@gmail.com>
2023-11-16 23:45:25 +0300

    modified the ui to show a strikethrough- based on the completed status of the task
    added a checkbox to mark task as completed or not
    
    added an urgency and category display to the tasks
    
    added a function to mark as completed base on checkbox

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ react-router-dom
 problems encountered:
 
 i had planned on using the splice function to delete the task from the tasks array but it was returning the deleted item instead of the new array, so i decided to use the filter function to remove the deleted item from the tasks array
+
+i needed to create a way to mark the tasks as completed, in the handleCompleted function i created a new array by mapping through the old array then checking if the index of the old array and the id of the task matches, and when the id and index matched i returned a new object that all the previous data but changes the completed status of previous object. then use the set function to change the old array into the new one
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -26,37 +26,52 @@ const Home = () => {
         },
         {
             title: "Clean House",
-            completed: false,
+            completed: true,
             description: "write about what happened today in your diary",
-            urgency: false,
+            urgency: true,
             category: "personal",
         },
     ]);
 
-    // this is a function that removes a task based on index of the tasl
+    // this is a function that removes a task based on index of the task
     const handleRemove = (id: number) => {
         const newArray = tasks.filter((items, index) => index !== id);
         setTasks(newArray);
     };
+    // this is a function that changes the value of the task to completed or not completed
+    const handleCompleted = (id: number) => {
+        const updatedData = tasks.map((obj, index) => {
+            if (index === id) return { ...obj, completed: !obj.completed };
+            return obj;
+        });
 
+        setTasks(updatedData);
+    };
+
+    // renders the list
+    const displayList = tasks.map((item, index) => (
+        <Task
+            key={item.title}
+            removeHandler={handleRemove}
+            completedHandler={handleCompleted}
+            id={index}
+            data={item}
+        />
+    ));
     return (
-        <div className='flex flex-col gap-5 py-10 px-5'>
-            <p className='text-center text-5xl font-bold'>To Do List</p>
-            <button className='flex items-center gap-5 w-full max-w-fit self-end text-white bg-[#20123A] px-3 py-1 rounded-md shadow-md'>
+        <div className='flex flex-col gap-5 md:gap-10 items-center py-10 px-3'>
+            <p className='text-center text-3xl md:text-5xl font-bold'>
+                To Do List
+            </p>
+            <button className='flex items-center gap-2 w-full max-w-fit self-end text-white bg-[#20123A] px-3 py-1 rounded-md shadow-md'>
                 <span className='text-3xl'>
                     <IoAddSharp />
                 </span>
                 <p className='w-full max-w-fit text-xl'>Add Tasks</p>
             </button>
-            <div>
-                {tasks.map((item, index) => (
-                    <Task
-                        key={item.title}
-                        removeHandler={handleRemove}
-                        id={index}
-                        data={item}
-                    />
-                ))}
+            <div className='flex flex-col gap-5 min-w-[320px] w-full px-3 md:px-5 max-w-[600px]'>
+                <p className='text-3xl md:text-5xl font-semibold px-5'>Tasks</p>
+                {displayList}
             </div>
         </div>
     );

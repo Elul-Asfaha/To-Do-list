dae3bec1413100c75d8d6a812f7d63d5b3dcc8a5
elul <scorpiondota22@gmail.com>
2023-11-17 18:29:05 +0300

    added 2 fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Agbalumo': ['Agbalumo', 'sans-serif'],
+        'Playpen': ['Playpen Sans', 'cursive']
+      },
+    },
   },
   plugins: [],
 }

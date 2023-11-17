2a2cb259843a3891c42c4bf91c5c3cb7bc6c8c06
elul <scorpiondota22@gmail.com>
2023-11-17 12:04:40 +0300

    added a functionality to the add tasks component

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ i had planned on using the splice function to delete the task from the tasks arr
 i needed to create a way to mark the tasks as completed, in the handleCompleted function i created a new array by mapping through the old array then checking if the index of the old array and the id of the task matches, and when the id and index matched i returned a new object that all the previous data but changes the completed status of previous object. then use the set function to change the old array into the new one
 
 i wanted to create a new component to display the tasks, which makes it easier to use the react-router-dom to switch between addTasks component and display component
+
+i had to create a separate input handler for each inputs so i can clearly define what type of event i am expecting from those inputs
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import DisplayTasks from "./DisplayTasks";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import AddTasks from "./AddTasks";
 type TasksType = {
     title: string;
     completed: boolean;
@@ -32,10 +33,17 @@ const Home = () => {
             category: "personal",
         },
     ]);
+    const [newTask, setNewTask] = useState({
+        title: "",
+        completed: false,
+        description: "",
+        urgency: false,
+        category: "",
+    });
 
     // this is a function that removes a task based on index of the task
     const handleRemove = (id: number) => {
-        const newArray = tasks.filter((items, index) => index !== id);
+        const newArray = tasks.filter((items, index) => index !== id && items);
         setTasks(newArray);
     };
     // this is a function that changes the value of the task to completed or not completed
@@ -48,8 +56,42 @@ const Home = () => {
         setTasks(updatedData);
     };
 
+    //this handles the new task
+    const handleNewTask = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target;
+        setNewTask({ ...newTask, [name]: value });
+    };
+    const handleNewTaskTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const { value, name } = e.target;
+        setNewTask({ ...newTask, [name]: value });
+    };
+    //this handles the new task checkbox
+    const handleNewTaskCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = e.target;
+        setNewTask({ ...newTask, [name]: checked });
+    };
+    const handleNewTaskSelectOption = (e: ChangeEvent<HTMLSelectElement>) => {
+        const { value, name } = e.target;
+        setNewTask({ ...newTask, [name]: value });
+    };
+
+    // this submits new task and redirect to the display tasks route
+    const navigate = useNavigate();
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        tasks.push(newTask);
+        setNewTask({
+            title: "",
+            completed: false,
+            description: "",
+            urgency: false,
+            category: "",
+        });
+        navigate("/");
+    };
     return (
-        <div className='flex flex-col gap-5 md:gap-10 items-center py-10 px-3'>
+        <div className='flex flex-col w-full px-5 gap-5 md:gap-10 items-center py-10'>
             <p className='text-center text-3xl md:text-5xl font-bold'>
                 To Do List
             </p>
@@ -61,6 +103,21 @@ const Home = () => {
                             tasks={tasks}
                             removeHandler={handleRemove}
                             completedHandler={handleCompleted}
+                        />
+                    }
+                />
+                <Route
+                    path='/CreateTasks'
+                    element={
+                        <AddTasks
+                            newTask={newTask}
+                            submitHandler={handleSubmit}
+                            newTaskHandler={handleNewTask}
+                            newTaskCheckboxHandler={handleNewTaskCheckbox}
+                            newTaskTextareaHandler={handleNewTaskTextarea}
+                            newTaskSelectOptionHandler={
+                                handleNewTaskSelectOption
+                            }
                         />
                     }
                 />
